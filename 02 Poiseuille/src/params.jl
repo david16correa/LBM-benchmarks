6bd752a04699fb524fb35fc8094ed4ef1cc9442b
David Correa <david.correa.bsc@gmail.com>
2025-01-08 20:41:02 -0600

    cleaning for the Winter Meeting 2025

--- 02 Poiseuille/src/params.jl
+++ 02 Poiseuille/src/params.jl
@@ -5,21 +5,21 @@ params
 ========================================================================================== =#
 
 # space
-xmax = 20 # mm
+xmax = 20 # μm
 x = range(-xmax, stop = xmax, step = 0.1);
 
 # fluid
-# water viscosity ≡ μ = 0.890 mPa s, water density ≡ ρ = 1000 kg/m³ → water kinematic shear viscosity ≡ ν ≡ μ/ρ = 0.890 (mm)²/(s),
-viscosity = 0.890 # (mm)²/(s)
+# water viscosity ≡ μ = 0.890 mPa s, water density ≡ ρ = 1000 kg/m³ → water kinematic shear viscosity ≡ ν ≡ μ/ρ = 0.890 (μm)²/(μs),
+viscosity = 0.890 # (μm)²/(μs)
 isFluidCompressible = false;
 
 # wall
-h = 10; # half width of channel, mm
+h = 10; # half width of channel, μm
 solidNodes = [j > h || j < -h for i in x, j in x];
 
 # pressure gradient
-forceDensity = [1e-4, 0.0] # water density units * mm/s²
+forceDensity = [1e-4, 0.0] # water density units * μm/μs²
 
 # simulation
-simulationTime = 1e3; # s
+simulationTime = 1e3; # μs
 ticksBetweenSaves = 100 |> snapshots -> simulationTime / step(x) / snapshots |> round |> Int64; # (about) 100 snapshots are saved
