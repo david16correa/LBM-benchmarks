ccdb6e61eda01a2c9c2262a654067a4336b18be6
David Correa <david.correa.bsc@gmail.com>
2025-01-08 20:43:06 -0600

    progress

--- 06 Stokeslet/README.md
+++ 06 Stokeslet/README.md
@@ -1,4 +1,4 @@
-# 05 Stokeslet
+# 06 Stokeslet
 
 Following [Graham (2018)](https://doi.org/10.1017/9781139175876), a stokeslet is the solution to a Stokes flow in an unbounded domain driven by a point force F exerted at the origin:
 ```math
--- 06 Stokeslet/src/params.jl
+++ 06 Stokeslet/src/params.jl
@@ -1,26 +1,26 @@
 #= ==========================================================================================
 =============================================================================================
-params - larger sphere
+params
 =============================================================================================
 ========================================================================================== =#
 
 # space
-xmax = 20 # μm
-x = range(-xmax, stop = xmax, step = 0.1); # μm
+xmax = 100 # μm
+x = range(-xmax, stop = xmax, step = 0.1);
+walledDimensions = [1,2];
 
 # fluid
 # water viscosity ≡ μ = 0.890 mPa s, water density ≡ ρ = 1000 kg/m³ → water kinematic shear viscosity ≡ ν ≡ μ/ρ = 0.890 (mm)²/(s),
 viscosity = 0.890 # (μm)²/(μs)
 isFluidCompressible = false;
 
-# particle
-massDensity = 1.; # mass density units
-radius = 4.; # μm
-position = [-10., 0]; # μm
-coupleTorques = false;
-coupleForces = true;
-angularVelocity = 1e-3; # μs⁻¹
+# stokeslet
+radius = 0.1; # μm
+position = [0., 0];
+force = [1e-3, 0.]; # mass density units * μm/μs²
+# a cylindrically normalized gaussian is used to approximate a Dirac delta
+stokeslet = [force * exp(-(i^2+j^2)/(2*radius^2))/(2*pi*radius^2) for i in x, j in x]; # mass density units * μm/μs²
 
 # simulation
 simulationTime = 1e3; # μs
-ticksBetweenSaves = 100 |> snapshots -> simulationTime / step(x) / snapshots |> round |> Int64; # (about) 100 snapshots are saved
+ticksBetweenSaves = 10 |> snapshots -> simulationTime / step(x) / snapshots |> round |> Int64; # (about) 500 snapshots are saved
